2ebce2ad6448ceb736436baf0f0ccfc275183531
iana-b <baraksina.ya@gmail.com>
2023-06-29 00:09:30 +0200

    Send photo

---
+++ main.py
@@ -0,0 +1,44 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types.input_file import InputFile
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv('API_TOKEN')
+UNSPLASH_API_KEY = os.getenv('UNSPLASH_API_KEY')
+UNSPLASH_URL = f'https://api.unsplash.com'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(f'Hi, {message.from_user.first_name}! 💕 \nWhat photo do you want? ')
+
+
+def get_photo(query):
+    url = f'{UNSPLASH_URL}/photos/random'
+    params = {
+        'query': query
+    }
+    headers = {
+        "Authorization": f"Client-ID {UNSPLASH_API_KEY}"
+    }
+    r = requests.get(url=url, params=params, headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+        return InputFile.from_url(data['urls']['small'])
+
+
+@dp.message_handler()
+async def send_answer(message: types.Message):
+    photo = get_photo(message.text)
+    await bot.send_photo(message.chat.id, photo)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
